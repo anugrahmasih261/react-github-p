b677ea889c53d4f3201761bb7de541da119d0193
Anugrah Masih <anugrahmasih@Anugrahs-MacBook-Pro.local>
2024-03-20 19:44:43 +0530

    Updates

---
+++ precache-manifest.b5a5b4739a6b04038a39de89f94af7a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "32e8c9663ccac3d778cb",
+    "url": "/react-github/static/css/main.be3c2bc2.chunk.css"
+  },
+  {
+    "revision": "32e8c9663ccac3d778cb",
+    "url": "/react-github/static/js/main.32e8c966.chunk.js"
+  },
+  {
+    "revision": "c36854d50bd2a57f4095",
+    "url": "/react-github/static/js/1.c36854d5.chunk.js"
+  },
+  {
+    "revision": "0af0a507f8e4cc7dfd23",
+    "url": "/react-github/static/js/2.0af0a507.chunk.js"
+  },
+  {
+    "revision": "02309506eb5dceba310d",
+    "url": "/react-github/static/js/runtime~main.02309506.js"
+  },
+  {
+    "revision": "d0abe3d3b60019c28e4847ca725aed12",
+    "url": "/react-github/static/media/main-background.d0abe3d3.png"
+  },
+  {
+    "revision": "d493b3a2edac1828971d4a26c55a69db",
+    "url": "/react-github/static/media/cart.d493b3a2.svg"
+  },
+  {
+    "revision": "284efebd589e06d14efaf5edf221eeb5",
+    "url": "/react-github/static/media/user.284efebd.svg"
+  },
+  {
+    "revision": "259b9a4558914059c3fe0384c7c0576e",
+    "url": "/react-github/index.html"
+  }
+];
